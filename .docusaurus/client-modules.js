7b00e7e96a155349753c2f0edd683bce7cd4d704
Milos Kriz <82968568+miloskriz@users.noreply.github.com>
2023-02-24 13:08:04 +0000

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\dev\\wiki\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\dev\\wiki\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\dev\\wiki\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\dev\\wiki\\src\\css\\custom.css'),
-];
