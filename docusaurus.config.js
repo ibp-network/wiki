ca8e79967e70e676506a571cf3ad142980b0f94c
Bluespottedfrog <thomas.tran789@gmail.com>
2023-05-24 03:56:40 -0700

    Update docusaurus config
    changed baseURL back to original

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,10 +12,10 @@ const config = {
 
   // Set the production url of your site here
   url: 'https://wiki.ibp.network',
-  
+
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/build/',
+  baseUrl: '/',
 
 
   // GitHub pages deployment config.
